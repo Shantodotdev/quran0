9c0e2db24804762eead89972221f98b4f7276347
KR Shanto <krshanto2005@gmail.com>
2026-06-10 11:10:53 +0600

    fix(eslint): update ignores list to include additional directories

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,12 @@ export default [
     },
   },
   {
-    ignores: ['eslint.config.js', 'prettier.config.js', 'public/**'],
+    ignores: [
+      '.output/**',
+      'dist/**',
+      'eslint.config.js',
+      'prettier.config.js',
+      'public/**',
+    ],
   },
 ]
